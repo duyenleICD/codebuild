8bfb2980fa08267cd2bfc98e833083463874e290
ICDVN-DUYENLH\User <duyenlh@icd-vn.com>
2023-02-03 09:18:04 +0700

    edit 123

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+app.get('/123', (req, res) => {
   res.send(
     `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
   );
